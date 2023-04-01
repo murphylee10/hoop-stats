db0d32ee5c83e41abda001841216dd8a27a5cc53
murphylee10 <murphylee2004@gmail.com>
2023-04-01 14:47:21 -0400

    flask skeleton

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import exc
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dkf3sldkjfDF23fLJ3b'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+
+app.run(debug=True, host='0.0.0.0', port=81)
